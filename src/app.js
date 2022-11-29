affdb7bb1fdfe35cc1069b15fed64e5dd47854b1
NeelamK1990 <neelamkatwal606@gmail.com>
2022-11-28 21:54:01 -0500

    updated my index.html

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
 
+let apiKey = "cf1ef184bc4f8c6749a14597b0b5efe6";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
