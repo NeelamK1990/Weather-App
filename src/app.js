4f3dc65be720a9d69646fd375b268f3eb438162d
NeelamK1990 <neelamkatwal606@gmail.com>
2022-11-29 20:16:54 -0500

    updated index.html and app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "cf1ef184bc4f8c6749a14597b0b5efe6";
