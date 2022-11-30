218c12b8faf2c14a9117912cd6ec11c1b48e4b87
NeelamK1990 <neelamkatwal606@gmail.com>
2022-11-29 21:47:31 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours < 0) {
+  if (hours < 10) {
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
