e47140b8e41663f3995d164d3c229bb7eb901fcd
NeelamK1990 <neelamkatwal606@gmail.com>
2022-12-06 11:11:42 -0500

    removed the console log

--- src/app.js
+++ src/app.js
@@ -64,7 +64,6 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "fe1483f743b581b5520a1b725af03a49";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
